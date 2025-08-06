763b48fbf4f0c1cea2c7d1db989ad58c4cab6520
Mahfuz Uddin <mahfux090@gmail.com>
2025-08-06 21:35:28 +0600

    CC

---
+++ Contest/Starters 198/Currency_Exchange.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios::sync_with_stdio(false); cin.tie(0);
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pb push_back
+#define ff first
+#define ss second
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
+#define sz(x) (int)(x).size()
+#define lb '\n'
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+const int INF = 1e9;
+const ll LINF = 1e18;
+const int MOD = 1e9+7;
+
+void solve() {
+    ll a1,b1,a2, b2;
+    cin>> a1 >> b1>>a2 >> b2;
+
+    ll v1 = 5*a1 + b1;
+    ll v2 = 5*a2+b2;
+
+    if (v1 >= v2 && (v1 - v2) % 6 == 0) {
+        cout << "YES" << lb;
+    } else {
+        cout << "NO" << lb;
+    }
+}
+
+int main() {
+    fast
+    int t=1;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
+//------------------------------------------//
+//          Mahfuz Uddin                    //
+//------------------------------------------//
