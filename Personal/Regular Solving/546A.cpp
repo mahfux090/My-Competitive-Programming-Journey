911792bcbb36b902ca02ae841a4c4455b8b5d109
Mahfuz Uddin <mahfux090@gmail.com>
2025-08-13 19:48:17 +0600

    CF

---
+++ Personal/Regular Solving/546A.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int k,n,w;
+    cin>>k>>n>>w;
+    int sum=0;
+    for (int i = 1; i <=w; i++) sum+=i;
+    int cost=k*sum;
+    if(cost<=n)
+        cout<<"0";
+    else
+        cout<<cost-n;
+    
+    return 0;
+}
---
+++ Personal/Regular Solving/977A.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    long long n;int k;cin>>n>>k;
+    while(k--){
+        if(n%10) n--;
+        else n/=10;
+    }
+    cout<<n;
+}
