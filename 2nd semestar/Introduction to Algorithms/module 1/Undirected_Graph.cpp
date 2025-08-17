aeb13e77d624d6773ce7acd644725f1304a2a7e5
Mahfuz Uddin <mahfux090@gmail.com>
2025-08-17 19:27:40 +0600

    CF

---
+++ 2nd semestar/Introduction to Algorithms/module 1/Undirected_Graph.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,e;
+    cin>>n>>e;
+    int mat[n][n];
+    memset(mat, 0, sizeof(mat));
+    while (n--)
+    {
+        int a,b;
+        cin>>a>>b;
+        mat[a][b]=1;
+        mat[b][a]=1;
+    }
+    
+    
+    
+    return 0;
+}
---
+++ 2nd semestar/Introduction to Algorithms/module 1/directed_Graph.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,e;
+    cin>>n>>e;
+    int mat[n][n];
+    memset(mat, 0, sizeof(mat));
+    while (n--)
+    {
+        int a,b;
+        cin>>a>>b;
+        mat[a][b]=1;
+    }
+    
+    
+    
+    return 0;
+}
