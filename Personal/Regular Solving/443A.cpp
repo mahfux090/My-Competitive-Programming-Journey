6275a2f44547197362f48d751e4cf5a799ffb41c
Mahfuz Uddin <mahfux090@gmail.com>
2025-08-18 20:15:11 +0600

    CF

---
+++ Personal/Regular Solving/443A.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    getline(cin,s);
+    cin>>s;
+    set<char>st;
+    for(char c:s)
+    {
+        if(isalpha(c)) st.insert(c);
+    }
+    cout<<st.size();
+    return 0;
+}
