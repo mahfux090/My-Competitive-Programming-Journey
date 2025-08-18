c6f0f031cb74dc787e63dbf5b2cbea69d728eb86
Mahfuz Uddin <mahfux090@gmail.com>
2025-08-18 21:05:21 +0600

    CF

--- Personal/Regular Solving/510A.cpp
+++ Personal/Regular Solving/510A.cpp
@@ -1,28 +1,24 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 int main()
 {
-    int n, m;
-    cin >> n >> m;
-    for (int i = 1; i <= n; i++)
-    {
-        if (i % 2 == 1)
-        {
-            for (int j = 1; j <= m; j++)
-                cout << "#";
-        }
-        else if ((i / 2) % 2 == 1)
-        {
-            for (int j = 2; j <= m; j++)
-                cout << ".";
-            cout << "#";
-        }
-        else
-        {
-            cout << "#";
-            for (int j = 1; j < m; j++)
-                cout << ".";
-        }
-        cout << endl;
+    int n,m;cin>>n>>m;
+    string s;
+    for (int i = 1; i < m; i++)
+        s.push_back('.');
+    s.push_back('#');
+    string t;
+    t.push_back('#');
+    for (int i = 1; i < m; i++)
+        t.push_back('.');
+    string w;
+    for (int i = 1; i <=m; i++)
+        w.push_back('#');
+    for (int i = 1; i <=n; i++) {
+        if(i%2==1)
+            cout<<w<<endl;
+        else if ((i/2)%2==1)
+            cout<<s<<endl;
+        else cout<<t<<endl;
     }
 }
