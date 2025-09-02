fb05159408cebe3b1112c6717f9ea4de32f840df
Mahfuz Uddin <mahfux090@gmail.com>
2025-09-02 17:48:59 +0600

    Cf

---
+++ 2nd semestar/Introduction to Algorithms/module 10.5/2104A.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int a,b,c;
+        cin>>a>>b>>c;
+        int sum=a+b+c;
+        if(sum%3==0 && sum/3>=b)
+            cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+    
+    return 0;
+}
---
+++ CF_1200_10Days/Day 03/459A.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios::sync_with_stdio(false); cin.tie(0);
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pb push_back
+#define ff first
+#define ss second
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
+#define sz(x) (int)(x).size()
+#define lb '\n'
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+const int INF = 1e9;
+const ll LINF = 1e18;
+const int MOD = 1e9+7;
+
+void solve() {
+    int x1,y1,x2,y2;
+    cin>>x1>>y1>>x2>>y2;
+
+    if(x1!=x2&&y1!=y2){
+        if(abs(x1-x2)!=abs(y1-y2)){
+            cout<<-1<<lb;
+        }
+        else{
+            cout<<x1<<" "<<y2<<" "<<x2<<" "<<y1<<lb;
+        }
+    }
+    else if(x1==x2){
+        int s=abs(y1-y2);
+        cout<<x1+s<<" "<<y1<<" "<<x2+s<<" "<<y2<<lb;
+    }
+    else{
+        int s=abs(x1-x2);
+        cout<<x1<<" "<<y1+s<<" "<<x2<<" "<<y2+s<<lb;
+    }
+}
+
+int main() {
+    fast
+    int t=1;
+    // cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
+//------------------------------------------//
+//          M a h f u z U d d i n           //
+//------------------------------------------//
