cc8c8168f2ee4d329287edfc03dcbe762ed8b5f0
Mahfuz Uddin <mahfux090@gmail.com>
2025-09-05 16:03:02 +0600

    cf

---
+++ CF_1200_10Days/Day 06/1692E.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios::sync_with_stdio(false); cin.tie(0);
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pb push_back
+#define ff first
+#define ss second
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
+#define sz(x) (int)(x).size()
+#define lb '\n'
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+const int INF = 1e9;
+const ll LINF = 1e18;
+const int MOD = 1e9+7;
+
+void solve() {
+    int n;
+    ll s;
+    cin>>n >> s;
+    vector<int> a(n);
+    ll sm = 0;
+    for(int i=0; i<n; i++){
+        cin >> a[i];
+        sm += a[i];
+    }
+
+    if(sm<s){
+        cout << -1 << lb;
+        return;
+    }
+    if(sm==s){
+        cout<<0<<lb;
+        return;
+    }
+
+    int l=0;
+    int mx=0;
+    ll cs=0;
+    for(int r=0; r<n; ++r){
+        cs += a[r];
+        while(cs > s){
+            cs -= a[l];
+            l++;
+        }
+        mx = max(mx, r-l+1);
+    }
+    cout << n-mx << lb;
+}
+
+int main() {
+    fast
+    int t=1;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
+//------------------------------------------//
+//          M a h f u z U d d i n           //
+//------------------------------------------//
