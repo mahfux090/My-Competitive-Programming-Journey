593c230c138bf8bdf72c266eeb8eda7eaf9dd2b1
Mahfuz Uddin <mahfux090@gmail.com>
2025-09-23 18:40:50 +0600

    cf

---
+++ Personal/Regular Solving/1512A.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios::sync_with_stdio(false); cin.tie(0);
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pb push_back
+#define ff first
+#define ss second
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
+#define sz(x) (int)(x).size()
+#define lb '\n'
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+const int INF = 1e9;
+const ll LINF = 1e18;
+const int MOD = 1e9+7;
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    if(a[0]!=a[1] && a[0]!=a[2]){
+        cout<<1<<lb;
+    }
+    else if(a[1]!=a[0] && a[1]!=a[2]){
+        cout<<2<<lb;
+    }
+    else{
+        for(int i=2;i<n;i++){
+            if(a[i]!=a[0]){
+                cout<<i+1<<lb;
+                break;
+            }
+        }
+    }
+}
+
+int main() {
+    fast
+    int t=1;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
+//------------------------------------------//
+//          M a h f u z U d d i n           //
+//------------------------------------------//
