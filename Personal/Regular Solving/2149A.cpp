adf6d3cf453a8e9e4ceebd15a92f29f39262e863
Mahfuz  Uddin <159001317+mahfux090@users.noreply.github.com>
2025-10-05 14:07:17 +0600

    Create 2149A.cpp

---
+++ Personal/Regular Solving/2149A.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios::sync_with_stdio(false); cin.tie(0);
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pb push_back
+#define ff first
+#define ss second
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
+#define sz(x) (int)(x).size()
+#define lb '\n'
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+const int INF = 1e9;
+const ll LINF = 1e18;
+const int MOD = 1e9+7;
+
+void solve() {
+    int n;
+    cin>>n;
+    int nc=0, zc=0;
+    for(int i=0; i<n; i++){
+        int v;
+        cin >> v;
+        if(v==0) zc++;
+        else if(v == -1) nc++;
+    }
+    int ops = zc;
+    if(nc % 2 != 0){
+        ops += 2;
+    }
+    cout<<ops<<lb;
+}
+
+int main() {
+    fast
+    int t=1;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
+//------------------------------------------//
+//          M a h f u z U d d i n           //
+//------------------------------------------//
