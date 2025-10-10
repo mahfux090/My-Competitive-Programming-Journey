5dd9cb05b1e730aa5ff4df6a3f6e006a6483491d
Mahfuz Uddin <mahfux090@gmail.com>
2025-10-10 11:43:38 +0600

    cf

---
+++ Personal/Regular Solving/1624A.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios::sync_with_stdio(false); cin.tie(0);
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pb push_back
+#define ff first
+#define ss second
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
+#define sz(x) (int)(x).size()
+#define lb '\n'
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+const int INF = 1e9;
+const ll LINF = 1e18;
+const int MOD = 1e9+7;
+
+void solve() {
+    int n;
+    cin >> n;
+    int mn = INF + 7;
+    int mx = 0;
+    for(int i=0; i<n; i++) {
+        int x;
+        cin >> x;
+        mn = min(mn, x);
+        mx = max(mx, x);
+    }
+    cout << mx - mn << lb;
+}
+
+int main() {
+    fast
+    int t=1;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
+//------------------------------------------//
+//          M a h f u z U d d i n           //
+//------------------------------------------//
