402d84797679dd6d3def528104a946f659e8a35a
Mahfuz Uddin <mahfux090@gmail.com>
2025-11-17 18:55:26 +0600

    cf

---
+++ Personal/1000 RT CF/1474B.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios::sync_with_stdio(false); cin.tie(0);
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pb push_back
+#define ff first
+#define ss second
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
+#define sz(x) (int)(x).size()
+#define lb '\n'
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+const int INF = 1e9;
+const ll LINF = 1e18;
+const int MOD = 1e9+7;
+
+bool isPrime(ll n){
+    if(n<2) return false;
+    for(ll i=2; i*i<=n; ++i){
+        if(n%i==0) return false;
+    }
+    return true;
+}
+
+void solve() {
+    ll d;
+    cin>>d;
+    ll a=1+d;
+    while(!isPrime(a)){
+        a++;
+    }
+    ll b=a+d;
+    while(!isPrime(b)){
+        b++;
+    }
+    cout<<a*b<<lb;
+}
+int main() {
+    fast
+    int t=1;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
+//------------------------------------------//
+//          M a h f u z U d d i n           //
+//------------------------------------------//
---
+++ Personal/1000 RT CF/1485A.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios::sync_with_stdio(false); cin.tie(0);
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pb push_back
+#define ff first
+#define ss second
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
+#define sz(x) (int)(x).size()
+#define lb '\n'
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+const int INF = 1e9;
+const ll LINF = 1e18;
+const int MOD = 1e9+7;
+
+void solve() {
+    ll a,b;
+    cin>>a>>b;
+    int ans=INF;
+    for(int k=0;k<40;++k){
+        ll nb=b+k;
+        if(nb==1) continue;
+        int ops=k;
+        ll ca=a;
+        while(ca>0){
+            ca/=nb;
+            ops++;
+        }
+        ans=min(ans,ops);
+    }
+    cout<<ans<<lb;
+}
+int main() {
+    fast
+    int t=1;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
+//------------------------------------------//
+//          M a h f u z U d d i n           //
+//------------------------------------------//
