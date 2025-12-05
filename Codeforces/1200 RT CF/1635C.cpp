c7eae0baf7fb83303d9c6cea4e6e455bf365012b
Mahfuz Uddin <mahfux090@gmail.com>
2025-12-05 18:44:40 +0600

    Update 1200

---
+++ Codeforces/1200 RT CF/1635C.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios::sync_with_stdio(false); cin.tie(0);
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pb push_back
+#define ff first
+#define ss second
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
+#define sz(x) (int)(x).size()
+#define lb '\n'
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+const int INF = 1e9;
+const ll LINF = 1e18;
+const int MOD = 1e9+7;
+
+void solve() {
+    int n;
+    cin>>n;
+    vector<ll> a(n);
+    for(int i=0;i<n;i++) cin >> a[i];
+    if(is_sorted(all(a))){
+        cout << 0 << lb;
+        return;
+    }
+    if(a[n-2]>a[n-1] || a[n-1]<0){
+        cout << -1 << lb;
+        return;
+    }
+    cout << n-2 << lb;
+    for(int i=1; i<=n-2; i++) {
+        cout << i << " " << n-1 << " " << n << lb;
+    }
+}
+
+int main() {
+    fast
+    int t=1;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
+//------------------------------------------//
+//          M a h f u z U d d i n           //
+//------------------------------------------//
