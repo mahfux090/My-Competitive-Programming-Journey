a07751d2a3e3cd2cbae511e8e39e36542856b5ff
Mahfuz Uddin <mahfux090@gmail.com>
2025-12-08 19:29:29 +0600

    Add 1200, Add 1200

---
+++ Codeforces/1200 RT CF/1178B.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fast ios::sync_with_stdio(false); cin.tie(0);
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pb push_back
+#define ff first
+#define ss second
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
+#define sz(x) (int)(x).size()
+#define lb '\n'
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+
+const int INF = 1e9;
+const ll LINF = 1e18;
+const int MOD = 1e9+7;
+
+void solve() {
+    string s;
+    cin>>s;
+    ll w=0;
+    int n=sz(s);
+    for(int i=0;i<n-1;i++){
+        if(s[i]=='v' && s[i+1]=='v') w++;
+    }
+    ll ans=0, cur=0;
+    for(int i=0;i<n;i++){
+        if(s[i]=='v' && i+1<n && s[i+1]=='v'){
+            cur++;
+        }
+        else if(s[i]=='o'){
+            ans+=(cur*(w-cur));
+        }
+    }
+    cout<<ans<<lb;
+}
+
+int main() {
+    fast
+    int t=1; 
+    // cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
