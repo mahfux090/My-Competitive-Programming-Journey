4d45303651974ef4056b3d0c01fcc0746925d5c9
Mahfuz Uddin <mahfux090@gmail.com>
2025-12-20 21:14:09 +0600

    Add 1400, Update 1400, Add 1400

---
+++ Codeforces/1400 RT CF/1215B.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios::sync_with_stdio(false); cin.tie(0);
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pb push_back
+#define ff first
+#define ss second
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
+#define sz(x) (int)(x).size()
+#define lb '\n'
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+const int INF = 1e9;
+const ll LINF = 1e18;
+const int MOD = 1e9+7;
+
+void solve() {
+    int n;
+    cin>>n;
+    ll p=1,ng=0;
+    int cur=1;
+    for(int i=0;i<n;i++){
+        int a;
+        cin>>a;
+        if(a<0)cur=-cur;
+        if(cur>0)p++;
+        else ng++;
+    }
+    cout<<p*ng<<" "<<p*(p-1)/2+ng*(ng-1)/2<<lb;
+}
+
+int main() {
+    fast
+    int t=1;
+    // cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
