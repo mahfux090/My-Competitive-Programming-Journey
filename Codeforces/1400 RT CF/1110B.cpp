c189403c4074cf21823b9716935f88ca5991f856
Mahfuz Uddin <mahfux090@gmail.com>
2025-12-22 11:13:35 +0600

    Add 1400, Add 1400

---
+++ Codeforces/1400 RT CF/1110B.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios::sync_with_stdio(false); cin.tie(0);
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pb push_back
+#define ff first
+#define ss second
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
+#define sz(x) (int)(x).size()
+#define lb '\n'
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+const int INF = 1e9;
+const ll LINF = 1e18;
+const int MOD = 1e9+7;
+
+void solve() {
+    int n, m, k;
+    cin>>n>>m>>k;
+    vector<int> b(n);
+    for(int i=0; i<n; i++) cin>>b[i];
+    
+    vector<int> g;
+    for(int i=0; i<n-1; i++){
+        g.pb(b[i+1]-b[i]-1);
+    }
+    
+    sort(all(g));
+    
+    ll ans = n;
+    for(int i=0; i<n-k; i++) {
+        ans += g[i];
+    }
+    cout << ans << lb;
+}
+
+int main() {
+    fast
+    int t=1; 
+    // cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
